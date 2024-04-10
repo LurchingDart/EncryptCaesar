11c76d80658a1aaf39f0b355cf440d65d057c6ae
LurchingDart <lurchdev@gmail.com>
2024-04-10 14:00:24 -0600

    Add new pages: Exam, Info, and Usage

---
+++ src/app/exam/page.tsx
@@ -0,0 +1,11 @@
+import { fontSpaceArmor } from "@/styles/fonts";
+
+export default function Exam() {
+    return (
+        <main className="flex flex-col h-screen items-center justify-center">
+            <h1 className={`${fontSpaceArmor.className} text-5xl`}>
+                Exam
+            </h1>
+        </main>
+    );
+}
---
+++ src/app/usage/page.tsx
@@ -0,0 +1,11 @@
+import { fontSpaceArmor } from "@/styles/fonts";
+
+export default function Usage() {
+    return (
+        <main className="flex flex-col h-screen items-center justify-center">
+                <h1 className={`${fontSpaceArmor.className} text-5xl`}>
+                    Usage
+                </h1>
+        </main>
+    );
+}
