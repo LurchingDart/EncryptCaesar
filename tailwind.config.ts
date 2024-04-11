e100435209276b2090912b9ab54cd0326960fc50
LurchingDart <lurchdev@gmail.com>
2024-04-11 00:11:53 -0600

    Update tailwind.config.ts to remove unused font sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,15 +17,6 @@ const config = {
         "2xl": "1400px",
       },
     },
-    fontSize: {
-      sm: '0.750rem',
-      base: '1rem',
-      xl: '1.333rem',
-      '2xl': '1.777rem',
-      '3xl': '2.369rem',
-      '4xl': '3.158rem',
-      '5xl': '4.210rem',
-    },
     fontWeight: {
       normal: '400',
       bold: '700',
