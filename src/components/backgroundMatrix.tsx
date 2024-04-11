47dce66aba59b1dac854b4c18f1e28ca176189a0
LurchingDart <lurchdev@gmail.com>
2024-04-11 00:13:50 -0600

    Add BackgroundMatrix component to create a matrix effect in the background

---
+++ src/components/backgroundMatrix.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+import { MatrixEffect } from "@/components/animations/matrixEffect";
+
+export function BackgroundMatrix() {
+    return (
+        <div className="h-lvh w-full relative">
+            <MatrixEffect
+                animationSpeed={5}
+                containerClassName="bg-transparent"
+                colors={[
+                    [77, 255, 207],
+                    [178, 255, 0],
+                ]}
+                opacities={[0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.4, 0.4, 0.4, 1]}
+                dotSize={2}
+            />
+        </div>
+    );
+}
